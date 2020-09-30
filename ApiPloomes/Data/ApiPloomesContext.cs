3e639cdd5470ad663817ea4e3fed4992dd070788
Lucas Calixto <lucascalixto180@gmail.com>
2020-09-30 20:45:41 -0300

    Funções iniciais + Procurar por nome

---
+++ ApiPloomes/Data/ApiPloomesContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiPloomes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.Data {
+    public class ApiPloomesContext : DbContext {
+        public ApiPloomesContext(DbContextOptions<ApiPloomesContext> options)
+       : base(options) {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    
+    }
+
+}
+
